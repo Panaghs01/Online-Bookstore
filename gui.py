63a1a845e7e825bb3a43c390271bbffaf85c36b8
June <166293934+junedayo@users.noreply.github.com>
2024-04-27 12:31:14 +0300

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,30 @@
+import sys
+import customtkinter as ctk
+from PIL import Image
+
+# ----------------------------- TKinter Settings -----------------------------#
+
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("green")
+
+root = ctk.CTk()
+
+root.title("BookStore")
+root.geometry('1200x600')
+
+logo = ctk.CTkImage(dark_image=Image.open(r'C:\Users\anton\Desktop\standard_test_images\ANTEIKU.jpg'))
+
+def on_close():
+    print("Closing application...")
+    root.destroy()
+    sys.exit()
+
+root.protocol("WM_DELETE_WINDOW", on_close)
+
+search_input = ctk.CTkEntry(root, placeholder_text='Search',
+                            justify='center').pack(pady=30)
+
+welcome = ctk.CTkLabel(root, text="Welcome! Today's offers...", 
+                       font=("Helvetica", 36, "bold")).pack(padx=200, pady=20)
+
+root.mainloop()
