b7d94e57c597af877e4c0b8d05d2923454a7be80
FieryInvader <45425690+FieryInvader@users.noreply.github.com>
2024-06-02 00:33:50 +0300

    SQL dump tweaked, minor additions to db connector
    Database table "has" got renamed to "inventory" for the sake of my sanity
    Books and admins added
    login_admin and add_book_to_cart methods added, not tested, waiting on gui progress for those to be testable
    Last commit also uploaded all the jpgs needed

--- bookstore.sql
+++ bookstore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Εξυπηρετητής: 127.0.0.1
--- Χρόνος δημιουργίας: 16 Μάη 2024 στις 11:57:36
+-- Χρόνος δημιουργίας: 01 Ιουν 2024 στις 23:30:06
 -- Έκδοση διακομιστή: 10.4.32-MariaDB
 -- Έκδοση PHP: 8.2.12
 
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Δομή πίνακα για τον πίνακα `admins`
+--
+
+CREATE TABLE `admins` (
+  `ID` int(10) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Άδειασμα δεδομένων του πίνακα `admins`
+--
+
+INSERT INTO `admins` (`ID`, `username`, `password`) VALUES
+(1, 'Chris', '123456'),
+(2, 'Antonis', 'qwerty'),
+(3, 'Sofia', '000000'),
+(4, 'Panos', '123123');
+
+-- --------------------------------------------------------
+
+--
 -- Δομή πίνακα για τον πίνακα `books`
 --
 
@@ -33,8 +55,28 @@ CREATE TABLE `books` (
   `author` varchar(30) NOT NULL,
   `publisher` varchar(30) NOT NULL,
   `genre` varchar(30) NOT NULL,
-  `cover` blob NOT NULL
+  `price` decimal(10,2) NOT NULL,
+  `cover` blob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Άδειασμα δεδομένων του πίνακα `books`
+--
+
+INSERT INTO `books` (`ISBN`, `title`, `author`, `publisher`, `genre`, `price`, `cover`) VALUES
+('01', 'Kafka on the Shore', 'Murakami Haruki', 'Vintage Books', 'Literature', 12.99, 0x6b61666b615f6f6e5f7468655f73686f72652e6a7067),
+('02', '1Q84 Books 1 and 2', 'Murakami Haruki', 'Vintage Books', 'Literature', 13.99, 0x317138345f312e6a7067),
+('03', '1Q84 Book 3', 'Murakami Haruki', 'Vintage Books', 'Literature', 13.99, 0x317138345f332e6a7067),
+('04', 'Kokoro', 'Natsume Soseki', 'Vintage Books', 'Literature', 11.99, 0x6b6f6b6f726f2e6a7067),
+('05', 'Nakahara Chuuya Poems', 'Nakahara Chuuya', 'Gracewing', 'Poems', 16.99, 0x6368757579615f706f656d732e6a7067),
+('06', '100 poems, 100 authors', '-', 'Penguin Classics', 'Poems', 12.99, 0x706f656d732e6a7067),
+('07', 'Goodnight Punpun Volume 1', 'Asano Inio', 'VIZ Media LLC', 'Manga', 16.99, 0x70756e5f312e6a7067),
+('08', 'Goodnight Punpun Volume 2', 'Asano Inio', 'VIZ Media LLC', 'Manga', 16.99, 0x70756e5f322e6a7067),
+('09', 'Goodnight Punpun Volume 3', 'Asano Inio', 'VIZ Media LLC', 'Manga', 16.99, 0x70756e5f332e6a7067),
+('10', 'JOJO Stone Ocean Volume 1', 'Araki Hirohiko', 'VIZ Media LLC', 'Manga', 12.99, 0x73746f6e655f6f6365616e5f312e6a6770),
+('11', 'JOJO Stone Ocean Volume 2', 'Araki Hirohiko', 'VIZ Media LLC', 'Manga', 12.99, 0x73746f6e655f6f6365616e5f322e6a6770),
+('12', 'JOJO Stone Ocean Volume 3', 'Araki Hirohiko', 'VIZ Media LLC', 'Manga', 12.99, 0x73746f6e655f6f6365616e5f332e6a6770),
+('13', 'Snow Country', 'Kawabata Yasunari', 'Knopf Doubleday Publishing Gro', 'Literature', 9.99, 0x736e6f775f636f756e7472792e6a7067);
 
 -- --------------------------------------------------------
 
@@ -59,24 +101,28 @@ CREATE TABLE `buys` (
 CREATE TABLE `customers` (
   `id` int(12) NOT NULL,
   `name` varchar(30) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `country` varchar(30) NOT NULL,
   `city` varchar(30) NOT NULL,
   `street` varchar(30) NOT NULL,
   `street_num` varchar(3) NOT NULL,
-  `postal_code` varchar(10) NOT NULL
+  `postal_code` varchar(10) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  `email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Δομή πίνακα για τον πίνακα `has`
+-- Δομή πίνακα για τον πίνακα `inventory`
 --
 
-CREATE TABLE `has` (
-  `Inventory_ID` int(10) NOT NULL,
-  `store_id` int(10) NOT NULL,
+CREATE TABLE `inventory` (
+  `Inventory_ID` int(255) NOT NULL,
   `book_ISBN` varchar(12) NOT NULL,
-  `quantity` int(30) NOT NULL
+  `store_ID` int(10) NOT NULL,
+  `quantity` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -125,6 +171,12 @@ CREATE TABLE `supplier` (
 --
 
 --
+-- Ευρετήρια για πίνακα `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- Ευρετήρια για πίνακα `books`
 --
 ALTER TABLE `books`
@@ -146,12 +198,12 @@ ALTER TABLE `customers`
   ADD PRIMARY KEY (`id`);
 
 --
--- Ευρετήρια για πίνακα `has`
+-- Ευρετήρια για πίνακα `inventory`
 --
-ALTER TABLE `has`
+ALTER TABLE `inventory`
   ADD PRIMARY KEY (`Inventory_ID`),
   ADD KEY `book_ISBN` (`book_ISBN`),
-  ADD KEY `store_id` (`store_id`);
+  ADD KEY `store_ID` (`store_ID`);
 
 --
 -- Ευρετήρια για πίνακα `sells`
@@ -179,6 +231,12 @@ ALTER TABLE `supplier`
 --
 
 --
+-- AUTO_INCREMENT για πίνακα `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT για πίνακα `buys`
 --
 ALTER TABLE `buys`
@@ -191,10 +249,10 @@ ALTER TABLE `customers`
   MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT για πίνακα `has`
+-- AUTO_INCREMENT για πίνακα `inventory`
 --
-ALTER TABLE `has`
-  MODIFY `Inventory_ID` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `inventory`
+  MODIFY `Inventory_ID` int(255) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT για πίνακα `sells`
@@ -227,11 +285,11 @@ ALTER TABLE `buys`
   ADD CONSTRAINT `buys_ibfk_3` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Περιορισμοί για πίνακα `has`
+-- Περιορισμοί για πίνακα `inventory`
 --
-ALTER TABLE `has`
-  ADD CONSTRAINT `has_ibfk_1` FOREIGN KEY (`book_ISBN`) REFERENCES `books` (`ISBN`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `has_ibfk_2` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `inventory`
+  ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`book_ISBN`) REFERENCES `books` (`ISBN`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `inventory_ibfk_2` FOREIGN KEY (`store_ID`) REFERENCES `store` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Περιορισμοί για πίνακα `sells`
--- database_connector.py
+++ database_connector.py
@@ -1,17 +1,39 @@
+"""
+
+Implemented functions: 
+    searching books
+    signup and login for user
+
+feel free to fuck around with what each function returns, to fit whatever 
+
+the giu.py code requires
+
+e.g. you could make search books return only the name or id of the book
+or tell me to change it however you like 
+
+-xrhstos
+
+"""
+
 import mysql.connector as sql
 
-database = sql.connect(user = "root", host = "localhost", database = "bookstore")
+database = sql.connect(user = "root", host = "localhost",
+                       database = "bookstore")
 
 cursor = database.cursor()
 
 #returns books that are like user_input, takes optional argument columns(tuple)
 #which is default as * and returns everything for that book
 def search_books(user_input, columns="*"): 
-    cursor.execute(f"SELECT {columns} FROM books WHERE title LIKE '%{user_input}%'")
+    cursor.execute(
+        f"SELECT {columns} FROM books WHERE title LIKE '%{user_input}%'")
     result = cursor.fetchall() 
     #result is a list of tuples that contains the SQL query result
     return result
 
+
+#this is not yet called in gui.py, need to implement more gui elements to get
+#all the necessary values for a new user
 def signup_user(name, username, password, country, city, street, street_number,
                 postal_code, phone, email):
     cursor.execute(f"SELECT username FROM customers")
@@ -20,5 +42,49 @@ def signup_user(name, username, password, country, city, street, street_number,
     emails = cursor.fetchall()
     if username in usernames: return "Username already in use"
     if email in emails: return "Email already in use"
-    cursor.execute(f"INSERT INTO customers (name, username, password, country, city, street, street_num, postal_code, phone, email) VALUES ({name}, {username}, {password}, {country}, {city}, {street}, {street_number}, {postal_code}, {phone}, {email})")
+    #ugly fucking line of code, tell me if u know how to make it not be so long
+    cursor.execute(
+        f"INSERT INTO customers (name, username, password, country, city, street, street_num, postal_code, phone, email) VALUES ({name}, {username}, {password}, {country}, {city}, {street}, {street_number}, {postal_code}, {phone}, {email})")
     
+#this is also not yet called in gui.py, im letting it up to you to decide 
+#when and how to call it    
+def login_user(username, password):
+    cursor.execute("SELECT username FROM customers")
+    usernames = cursor.fetchall()
+    if username not in usernames:
+        return 1 #Invalid username error
+    else:
+        cursor.execute(
+            f"SELECT password FROM customers WHERE username={username}")
+        passwords = cursor.fetchall()
+        if password not in passwords: return 2 #Invalid password error
+        else: return 0 #login sucessful
+        
+
+def login_admin(username, password):
+    cursor.execute("SELECT username FROM admins")
+    usernames = cursor.fetchall()
+    if username not in usernames:
+        return 1 #Invalid username error
+    else:
+        cursor.execute(
+            f"SELECT password FROM admins WHERE username={username}")
+        passwords = cursor.fetchall()
+        if password not in passwords: return 2 #Invalid password error
+        else: return 0 #login sucessful
+        
+#this function takes book_isbn and optionally a value that is default to 1
+#and returns if book can be added to cart or if it is out of stock
+#THIS HAS NOT BEEN TESTED YET, if statement might need minor adjustments
+def add_book_to_cart(isbn,quantity=1):
+    cursor.execute(f"SELECT quantity FROM inventory WHERE book_ISBN={isbn}")
+    inventory = cursor.fetchall()
+    if inventory[0][0] - quantity >= 0: return 0 #Success
+    else: return 1 #Not enough copies of the book in stock
+
+
+
+
+
+
+
