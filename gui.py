88837ba49b8ce1c085748636dd7b9dd2b65a4d1e
sofinio <sofiakaradimitriou1@gmail.com>
2024-06-11 18:12:27 +0300

    FIRST COMMIT!!!!!!!!!!

--- gui.py
+++ gui.py
@@ -16,7 +16,7 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("green")
-
+#I KMS
 root = ctk.CTk()
 
 root.title("BookStore")
