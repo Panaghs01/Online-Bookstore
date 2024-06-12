ec958b6ff321dbb5dc40546e35575a2710146c54
Panaghs131 <125392587+Panaghs131@users.noreply.github.com>
2024-06-12 16:01:37 +0300

    Completed top3 most popular book and book recommendation
    Check for bugs and connect to DB then check again

--- ML.py
+++ ML.py
@@ -26,17 +26,61 @@ te = TransactionEncoder()
 te_ary = te.fit(dataset).transform(dataset)
 df = pd.DataFrame(te_ary, columns= te.columns_, index = ids)
 
-print(df,'\n')
+#print(df,'\n')
 
 ap = apriori(df,min_support= 0.1,use_colnames=True) #run apriori with 10% minimum support
-print(ap,'\n')
 
-print("TOP 3 BOOKS WITH MINIMUM SUPPORT 0.5")
-ap2 = apriori(df,min_support=0.5,use_colnames=True,max_len=1)
-print(ap2.head(3).sort_values('support',ascending = False))  #Top 3 books
+def top3():
+
+    #print("TOP 3 BOOKS WITH MINIMUM SUPPORT 0.5")
+    ap2 = apriori(df,min_support=0.5,use_colnames=True,max_len=1)
+    ap2 = ap2.head(3).sort_values('support',ascending = False)  #Top 3 books
+    #print(ap2)
+    books = [next(iter(x)) for x in ap2.itemsets]
+    return books
+
+#print(top3())
+
+def recommendations(cart):
+    cart = set(cart)
+    a = []
+    recommendation = []
+    rules = association_rules(ap,metric='confidence',min_threshold=0.5)
+    rules['antecedent_len'] = rules["antecedents"].apply(lambda x: len(x))
+    rules = rules[["antecedents","consequents","lift","antecedent_len"]]
+    print(rules,'\n')
+    if rules[(rules['antecedents'] == cart) & (rules['lift'] > 1)].empty:
+        for i in cart:
+            rules2 = rules[(rules['antecedents'] == {i}) & (rules['lift'] > 1)]
+            rules2 = rules2['consequents']
+            recommendation.append(next(iter(rules2.values)))
+        for i in recommendation:
+            i = next(iter(i))
+            a.append(i)
+        return set(a)        
+    else:
+        rules = rules[rules['antecedents'] == cart]
+        rules = rules.sort_values(by = ['lift'])
+        result = rules['consequents'].head(1)
+        return set(next(iter(result)))
+
+    
+print(recommendations([1,2]))
 
 
-rules = association_rules(ap,metric='confidence',min_threshold=0.5)
-rules = rules[['antecedents','consequents','confidence','lift']]
 
-print(rules.sort_values(by = 'lift',ascending = False))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
