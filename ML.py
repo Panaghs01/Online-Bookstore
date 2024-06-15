602b48d274c785511dbfd6cdab0102257088a3dc
Panos <162453326+Panaghs01@users.noreply.github.com>
2024-06-15 22:41:04 +0300

    Finished ML i think.

--- ML.py
+++ ML.py
@@ -1,16 +1,14 @@
 import numpy as np 
 import pandas as pd 
 #pip install mlxtend
-import mlxtend
-from mlxtend.frequent_patterns import apriori
-from mlxtend.frequent_patterns import association_rules
+from mlxtend.frequent_patterns import apriori, association_rules 
 from mlxtend.preprocessing import TransactionEncoder
 import database_connector as DB
+import random
 
 pd.set_option('display.max_rows', None)
 
 transactions = DB.return_transactions()
-#print(transactions)
 
 #DATASET = BOOKS ISBNs OF EACH TRANSACTION
 dataset = transactions
@@ -20,8 +18,6 @@ dataset = transactions
 te = TransactionEncoder()
 te_ary = te.fit(dataset).transform(dataset)
 df = pd.DataFrame(te_ary, columns= te.columns_)
-
-# print(df,'\n')
 
 ap = apriori(df,min_support= 0.1,use_colnames=True) #run apriori with 10% minimum support
 
@@ -33,56 +29,46 @@ def top3():
     books = [next(iter(x)) for x in ap2.itemsets]   #Un-frozenset-ing
     return books
 
-#print(top3())
-
 def recommendations(cart):
-
+    #Cart needs to be cast as set to work with the association rules
     cart = set(cart)
-    a = []
     recommendation = {}
     rules = association_rules(ap,metric='lift')
     rules = rules[["antecedents","consequents","lift","confidence"]]
     rules['consequents_len'] = rules['consequents'].apply(lambda x: len(x))
+
+    #Getting the required fields
+
     if rules[(rules['antecedents'] == cart) & (rules['lift'] > 1)].empty:
+        #If we cannot find any itemset that is in cart, check for each item in the cart
+        #Then store the lift and the item in a dictionary
+        #If the dictionary is empty at the end it means we don't have any good data
+        #About the currect item so return a random book instead
         for i in cart:
             rules2 = rules[
                 (rules['antecedents'] == {i}) &
-                (rules['lift'] > 1) &
+                (rules['lift'] > 1) & 
                 (rules['consequents_len'] == 1)]
             rules2 = rules2.sort_values(by = 'lift' , ascending = False).head(1)
-            # print(rules2)
             if not rules2['lift'].empty:
                 recommendation[i] = rules2['lift'].item()
-            # print(recommendation)
-        # rules2 = rules2.sort_values(by = 'lift', ascending = False)
-        # rules2 = rules2['consequents']
-        recommendation['1'] = 1.0234
-        recommendation = sorted(recommendation.items(), key = lambda x: x[1],reverse = True)
-        return recommendation[0][0]
+        if recommendation:
+            recommendation = sorted(recommendation.items(),reverse= True)
+            rules = rules[(rules['antecedents'] == {recommendation[0][0]}) &
+                          (rules['lift'] > 1) &
+                          (rules['consequents_len'] == 1)]
+            rules = rules.sort_values(by = 'lift',ascending = False)
+            rules = rules['consequents'].head(1)
+            a = rules.item()
+            return next(iter(a))
+        else:
+            return random.randint(1,13)
+        
     else:
+        #If there is a valid association rules about all items in cart return the consequent
         rules = rules[rules['antecedents'] == cart]
         rules = rules.sort_values(by = ['lift'], ascending = False)
         rules = rules[(rules['consequents_len'] == 1)].head(1)
         result = rules['consequents'].item()
         return next(iter(result))
-
-    
-#print(recommendations(['13','07']))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
